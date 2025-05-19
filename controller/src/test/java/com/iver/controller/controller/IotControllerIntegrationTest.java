7c2aa0f6152b268bf6407cb04b9eb41b1ae59103
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2025-05-19 03:03:06 +0300

    delete container tests

--- controller/src/test/java/com/iver/controller/controller/IotControllerIntegrationTest.java
+++ controller/src/test/java/com/iver/controller/controller/IotControllerIntegrationTest.java
@@ -1,37 +1,37 @@
-package com.iver.controller.controller;
-
-import com.iver.controller.AbstractIntegrationTest;
-import com.iver.controller.model.DevicePackage;
-import com.iver.controller.repository.DevicePackageRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-class IotControllerIntegrationTest extends AbstractIntegrationTest {
-
-    @Autowired
-    DevicePackageRepository devicePackageRepository;
-
-    @Autowired
-    IotController iotController;
-
-    @Test
-    void processDeviceData_validInput_success() {
-        String validJson = "{\"temperature\":25}";
-        String deviceId = "device-123";
-
-        iotController.executeIotPackage(deviceId, validJson);
-
-        var allSavedPackages = devicePackageRepository.findAll();
-
-        assertEquals(allSavedPackages.size(), 1);
-        assertEquals(allSavedPackages.getFirst().getDeviceId(), deviceId);
-        assertEquals(allSavedPackages.getFirst().getDeviceData(), validJson);
-    }
-}
+//package com.iver.controller.controller;
+//
+//import com.iver.controller.AbstractIntegrationTest;
+//import com.iver.controller.model.DevicePackage;
+//import com.iver.controller.repository.DevicePackageRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.ArgumentMatchers.anyString;
+//import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.when;
+//
+//class IotControllerIntegrationTest extends AbstractIntegrationTest {
+//
+//    @Autowired
+//    DevicePackageRepository devicePackageRepository;
+//
+//    @Autowired
+//    IotController iotController;
+//
+//    @Test
+//    void processDeviceData_validInput_success() {
+//        String validJson = "{\"temperature\":25}";
+//        String deviceId = "device-123";
+//
+//        iotController.executeIotPackage(deviceId, validJson);
+//
+//        var allSavedPackages = devicePackageRepository.findAll();
+//
+//        assertEquals(allSavedPackages.size(), 1);
+//        assertEquals(allSavedPackages.getFirst().getDeviceId(), deviceId);
+//        assertEquals(allSavedPackages.getFirst().getDeviceData(), validJson);
+//    }
+//}
